69e8566c1f0d28679c47d42e53a560d930d9c820
elder <lopez_felix_23@hotmail.com>
2024-11-26 17:34:53 -0600

    error Handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { provideRouter } from '@angular/router';
+
+import { errorHandlingInterceptor } from './interceptor/error-handling.interceptor'
 
 import { routes } from './app.routes';
 
@@ -8,5 +10,5 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient()]
+    provideHttpClient(withInterceptors([errorHandlingInterceptor]))]
 };
