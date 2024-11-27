370ed832eca65ff3b471a3343b73c21a9c385d5f
elder <lopez_felix_23@hotmail.com>
2024-11-26 18:26:47 -0600

    error

--- src/app/interceptor/error-handling.interceptor.ts
+++ src/app/interceptor/error-handling.interceptor.ts
@@ -1,5 +1,22 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 export const errorHandlingInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  return next(req).pipe(
+    catchError(error=> {
+      let errorMessage = '';
+        if (error instanceof ErrorEvent) {
+          // client-side error
+          errorMessage = `Client-side error: ${error.error.message}`;
+        } else {
+          // backend error
+          errorMessage = `Server-side error: ${error.status} ${error.message}`;
+        }
+
+        console.log(errorMessage);
+        
+
+        return throwError(errorMessage);
+    })
+  );
 };
